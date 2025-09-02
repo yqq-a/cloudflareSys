e0aef464e831abcca4baac3a4091f38e72b4b722
yqq-a <qiuchanye7@gmail.com>
2025-09-02 10:48:13 +0800

    ⚛️ 添加 React 主入口文件

---
+++ pages/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AIChatPage from './components/AIChatPage.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AIChatPage />
+  </React.StrictMode>,
+)
